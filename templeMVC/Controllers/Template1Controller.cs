0882918cc67aad5e452ce59679564b1b9b53938a
ERic <n88068@gmail.com>
2020-09-13 23:55:57 +0800

    0913

---
+++ templeMVC/Controllers/Template1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace templeMVC.Controllers
+{
+    public class Template1Controller : Controller
+    {
+        // GET: Template1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
